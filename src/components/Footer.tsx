f959083ba91a28da7799803be23c5fc715ec1a4e
Omar Gonzalez <omar.gonzalez@leafgroup.com>
2024-01-04 01:44:08 -0800

    fix: break footer out of container so it stretches across the entire width of the browser

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 export default function Footer() {
+  const styles = {
+    width: '100vw',
+    margin: '0 0 0 -50vw',
+    left: '50%',
+    position: 'relative',
+  }
   return (
-    <footer className='bg-gray-900 text-white pl-10 pr-10 h-[80px] leading-[80px]'>
+    <footer className='footer bg-gray-900 text-white pl-10 pr-10 h-[80px] leading-[80px]'>
       &copy; {new Date().getFullYear()} s9tpepper.dev
     </footer>
   )
