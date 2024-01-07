11c9872fbb532cde1f21d5f845c1bf0ae3f47ab0
Omar Gonzalez <omar.gonzalez@leafgroup.com>
2024-01-07 03:29:27 -0800

    fix: favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='shortcut icon' href='/favicon-16x16.png' />
+      </head>
       <body>
         <NextSSRPlugin routerConfig={extractRouterConfig(fileRouter)} />
         {children}
